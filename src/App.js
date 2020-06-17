13dce293bb7bbea9fc55bb5c0a15678ed8f35e9a
muhammad-atique <51038936+muhammad-atique@users.noreply.github.com>
2020-06-18 01:42:46 +0500

    Solved Assignment # 1 React Guide

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import './App.css';
-import Hello from './Hello'
-import './hello.css'
+import Hello from './Hello';
+import './hello.css';
+import Hi from './Assignment';
 
 //Another way of writing the above function 
 const App = ({ name, age, space }) => < div className = "app_div" > Hello from { name }
 whose age is { age - 10 } { space }
 years old. <
-    br / > < Hello fullName = "Muhammad Atique. This is being imported as tree in App.js" > < /Hello> < /
-div > ;
+    br / > < Hello fullName = "Muhammad Atique. This is being imported as tree in App.js" > < /Hello>  <
+br / >
+    <
+    Hi myName = "Muhammad Atique Rafique" > < /Hi> < /
+    div > ;
 
 
 
---
+++ src/Assignment.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Hi = ({ myName }) => ( <
+    div > Hello < strong > { myName } < /strong>  <
+    br / >
+    <
+    h3 > This is Assignment# 1 from React Guide < /h3>  <
+    br / >
+    <
+    ol >
+    <
+    li > Class 3 < /li> <
+    li > Learning React < /li> <
+    li > http: //react-guide-backup.surge.sh/</li>
+    <
+    /ol>  <
+    br / >
+    <
+    div > Sum is { 5 + 10 } < /div> < /
+    div >
+)
+export default Hi;
