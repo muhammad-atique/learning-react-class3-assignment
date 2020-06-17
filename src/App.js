aea11bd111fa782b407c2cbea19b0fa91984a12a
muhammad-atique <51038936+muhammad-atique@users.noreply.github.com>
2020-06-17 23:31:40 +0500

    Correct commment in App.js class work

--- src/App.js
+++ src/App.js
@@ -12,44 +12,44 @@ div > ;
 
 
 
-{
-    /* 
-    function App({ name, age, space }) {
-        return <div >
-            Hello from { name }
-        whose age is { age - 10 } { space }
-        years old. <
-            /div>
-    }
-     */
-}
 
-{
-    /* 
-    //Another way of writing the above function 
-    const App = ({ name, age, space }) => < div > Hello from { name }
+/* 
+function App({ name, age, space }) {
+    return <div >
+        Hello from { name }
     whose age is { age - 10 } { space }
-    years old. < /div>;
-     */
+    years old. <
+        /div>
 }
+ */
 
-{
-    /* function App(props) {
-        return <div >
-            Hello from { props.name }
-        whose age is { props.age - 10 } { props.space }
-        years old. <
-            /div>
-    } */
-}
 
-{
-    /* function App() {
-        return <div >
-            Hello from App.js UPDATED <
-            div > Another Div < /div> <
-            /div>
-    } */
-}
+
+/* 
+//Another way of writing the above function 
+const App = ({ name, age, space }) => < div > Hello from { name }
+whose age is { age - 10 } { space }
+years old. < /div>;
+ */
+
+
+
+/* function App(props) {
+    return <div >
+        Hello from { props.name }
+    whose age is { props.age - 10 } { props.space }
+    years old. <
+        /div>
+} */
+
+
+
+/* function App() {
+    return <div >
+        Hello from App.js UPDATED <
+        div > Another Div < /div> <
+        /div>
+} */
+
 
 export default App;
